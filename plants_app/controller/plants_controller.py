d813f24347c1af9233679314fa5bebf4f7e7eb98
Brandon Thomas <bran_thom@yahoo.com>
2023-04-16 11:34:26 -0500

    CRUD done, need validation, link to one veg, desc

--- plants_app/controller/plants_controller.py
+++ plants_app/controller/plants_controller.py
@@ -1,11 +1,71 @@
 from plants_app import app
-from datetime import timedelta
+from datetime import timedelta,datetime
 from plants_app.models.plants_model import Plants
 from flask import render_template,session, request,redirect,session,flash
 
 @app.route('/')
 def show_plants():
     plants = Plants.get_all_plants()
-    for plant in plants:
-        plant.harvest_date = plant.when_planted + timedelta(days=plant.days_to_maturity)
+    # for plant in plants:
+    #     plant.harvest_date = plant.when_planted + timedelta(days=plant.days_to_maturity)
     return render_template("index.html", plants = plants)
+
+@app.route('/new_plant')
+def new_plant():
+    return render_template("new_plant.html")
+
+@app.route('/added_plant', methods= ['POST'])
+def added_plant():
+    when_planted = datetime.strptime(request.form['when_planted'], '%Y-%m-%d')
+    days_to_maturity = int(request.form['days_to_maturity'])
+    harvest_date = when_planted + timedelta(days=days_to_maturity)
+    
+    print("harvest date:", harvest_date)
+
+    plant_data = {
+        'name': request.form['name'],
+        'days_to_maturity': request.form['days_to_maturity'],
+        'when_planted': request.form['when_planted'],
+        'date_to_harvest': harvest_date,
+        'updated_at' : 'updated_at',
+        'created_at' :'created_at',
+        'number_of_packets': request.form['number_of_packets']
+    }
+    print("plant info:", plant_data)
+    Plants.save(plant_data)
+    return redirect('/')
+
+@app.route('/edit_plant/<int:id>')
+def edit_plant(id):
+    data = {'id': id}
+
+    plant = Plants.get_one_plant(data)
+    Plants.edit_plants(request.form)
+    return render_template('edit_plant.html', plant = plant)
+
+@app.route('/edited_plant/<int:id>', methods = ['POST'])
+def edited_plant(id):
+    when_planted = datetime.strptime(request.form['when_planted'], '%Y-%m-%d')
+    days_to_maturity = int(request.form['days_to_maturity'])
+    harvest_date = when_planted + timedelta(days=days_to_maturity)
+    data = {"id":id,
+        'name': request.form['name'],
+        'days_to_maturity': request.form['days_to_maturity'],
+        'when_planted': request.form['when_planted'],
+        'date_to_harvest': harvest_date,
+        'updated_at' : 'updated_at',
+        'created_at' :'created_at',
+        'number_of_packets': request.form['number_of_packets']
+        }
+    
+    Plants.edit_plants(data)
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete_plant(id):
+    data = {
+        'id': id
+    }
+    print(data)
+    Plants.delete(data)
+    return redirect('/')
--- plants_app/models/plants_model.py
+++ plants_app/models/plants_model.py
@@ -9,24 +9,41 @@ class Plants:
         self.name = data['name']
         self.days_to_maturity = data['days_to_maturity']
         self.when_planted = data['when_planted']
-        self.date_to_harvest = None
+        self.date_to_harvest = data['date_to_harvest']
+        self.number_of_packets = data['number_of_packets']
         self.updated_at = data['updated_at']
         self.created_at = data['created_at']
 
 
-# @classmethod
-#     def save(cls, data):
-#         query = "INSERT INTO plants (name, email,password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, now(),now());"
-#         return connectToMySQL('dragrace').query_db(query, data)
+    @classmethod
+    def save(cls, plant_data):
+        query = "INSERT INTO plants (name, days_to_maturity,when_planted, date_to_harvest, number_of_packets,created_at, updated_at) VALUES (%(name)s, %(days_to_maturity)s, %(when_planted)s, %(date_to_harvest)s, %(number_of_packets)s, current_timestamp(),current_timestamp());"
+        return connectToMySQL('plants_inventory').query_db(query, plant_data)
 
-
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM plants WHERE plants.id = %(id)s";
+        return connectToMySQL('plants_inventory').query_db(query, data)
+    
     @classmethod
     def get_all_plants(cls):
         query = "select * from plants;"
         results = connectToMySQL('plants_inventory').query_db(query)
         plants = []
         for result in results:
-            result = cls(results[0])
+            result = cls(result)
             plants.append(result)
-        print(plants)
         return plants
+    
+    @classmethod
+    def get_one_plant(cls,data):
+        query = "select * from plants where plants.id =%(id)s;"
+        results = connectToMySQL('plants_inventory').query_db(query,data)
+        print(results)
+        results = cls(results[0])
+        return results
+    
+    @classmethod
+    def edit_plants(cls, data):
+        query = "UPDATE plants SET name = %(name)s, days_to_maturity = %(days_to_maturity)s, when_planted = %(when_planted)s, date_to_harvest = %(date_to_harvest)s, number_of_packets = %(number_of_packets)s,updated_at=current_timestamp() WHERE plants.id = %(id)s";
+        return  connectToMySQL('plants_inventory').query_db(query,data)
